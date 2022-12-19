31aa7e5175ee25ed00f18dc2e3025e91214fd6e8
Cristian Amodeo <[cristian.amodeo@gmail.com]>
2022-12-19 09:59:17 -0300

    V1

---
+++ Desafio5_motores_plantillas/ejs/routes/productos.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const router = Router();
+
+let productos = [];
+
+router.get('/', (req, res) => res.render('productos', { productos }));
+
+router.get('/productos', (req, res) => res.render('formularioProducto'));
+
+router.post('/productos', (req, res) => {
+	const { nombre, precio, url } = req.body;
+	productos.push({ nombre, precio, url });
+	res.render('formularioProducto');
+});
+
+module.exports = router;
